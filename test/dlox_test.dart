a3e249e853fc2233d1f96a1a10538c83d331370c
Sahil Sonawane <iamsahilsonawane@gmail.com>
2025-05-10 19:52:20 +0530

    test: add a small test

--- test/dlox_test.dart
+++ test/dlox_test.dart
@@ -1,4 +1,12 @@
 import 'package:dlox/dlox.dart';
 import 'package:test/test.dart';
 
-void main() {}
+void main() {
+  group("static analysis - resolver", () {
+    test("unused var", () async {
+      DLox dlox = DLox();
+      await dlox.runFile("./examples/unused_var.dlox");
+      expect(DLox.hadError, true);
+    });
+  });
+}
